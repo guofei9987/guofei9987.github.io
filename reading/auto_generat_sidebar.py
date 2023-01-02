de9c1a1f00ba7f0364f14b2749a51c013110eef8
robot <robot@guofei.site>
2023-01-03 00:12:50 +0800

    update

--- reading/auto_generat_sidebar.py
+++ reading/auto_generat_sidebar.py
@@ -115,13 +115,13 @@ with open('media/reading_words.json', 'w') as f:
 
 # %% 读书数量
 
-with open('media/book_cnt.svg', 'w') as f:
-    with open('../pages/book_list.json') as f2:
-        book_list = json.load(f2)
-    f.write('''<svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" width="58" height="22" role="img">
-  <text x="1" y="20" style="fill:red;">{}本
-  </text>
-</svg>'''.format(len(book_list)))
+# with open('media/book_cnt.svg', 'w') as f:
+#     with open('../pages/book_list.json') as f2:
+#         book_list = json.load(f2)
+#     f.write('''<svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" width="58" height="22" role="img">
+#   <text x="1" y="20" style="fill:red;">{}本
+#   </text>
+# </svg>'''.format(len(book_list)))
 
 # %% 侧边栏
 head = '''
