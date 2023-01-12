adef7b9ef3d83427f8502b24ef559135ec7f2079
robot <robot@guofei.site>
2023-01-12 21:09:16 +0800

    update

--- reading/auto_generat_sidebar.py
+++ reading/auto_generat_sidebar.py
@@ -1,17 +1,12 @@
 # -*- coding: utf-8 -*-
 
-
-# print sidebar & detail list
-# 我另外写了一个多叉树实现的版本。
-# 我的目录结构都是两级，且都在docs下，所以也就不需要多叉树了。
-
-# %%
 import os
 import re
 import string
 import json
+import requests
 
-# 字数统计
+# %% 字数统计
 regex_chinese = re.compile('[\u4e00-\u9fa5]')  # 汉字
 regex_English = re.compile('[0-9a-zA_Z]+')  # 数字和英语单词
 # 去掉中文标点和英文标点
@@ -37,6 +32,9 @@ def word_count(file_name_md):
     title_level_2 = [line.replace('## ', '').replace('\n', '') for line in passages if line.startswith('## ')]
     f.close()
     return word_num, title_level_2
+
+
+# %%
 
 
 # %%
@@ -95,45 +93,22 @@ print(detail)
 total_words = sum(i[2] for i in data)
 print('_' * 10, 'total words = {}'.format(total_words), '_' * 10)
 
-# %%总字数svg
+# %%获取：总star数量
 
-# total_words
-# # 写入 svg
-# with open('media/reading_words.svg', 'w') as f:
-#     f.write('''<svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" width="67" height="22" role="img">
-#   <text x="1" y="20" style="fill:red;">{}万字
-#   </text>
-# </svg>'''.format(round(total_words / 10000, ndigits=1)))
-#
-# # 写入 json
-# reading_words = {"reading_words": "{}万字".format(round(total_words / 10000, ndigits=1)),
-#                  "reading_words_accurate": total_words}
-# import json
-#
-# with open('media/reading_words.json', 'w') as f:
-#     json.dump(reading_words, f, ensure_ascii=False, indent='')
 
+r = requests.get('https://www.guofei.site/guofei9987/achievement.json')
+achievement = json.loads(r.content.decode('utf-8'))
 
 # %%总字数写入到 _data 里面
 import json
 
 with open('../_data/cnt_reading_words.json', 'w') as f:
     json.dump({"cnt_reading_words_precision": total_words,
-               "cnt_reading_words": "{}万字".format(round(total_words / 10000, ndigits=1))}
+               "cnt_reading_words": "{}万字".format(round(total_words / 10000, ndigits=1)),
+               "github_total_star_str": achievement['github_total_star_str']
+               }
               , f, ensure_ascii=False, indent='')
 
-# %% 读书数量
-
-# with open('media/book_cnt.svg', 'w') as f:
-#     with open('../pages/book_list.json') as f2:
-#         book_list = json.load(f2)
-#     f.write('''<svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" width="58" height="22" role="img">
-#   <text x="1" y="20" style="fill:red;">{}本
-#   </text>
-# </svg>'''.format(len(book_list)))
-
-
-import requests
 
 r = requests.get('https://www.guofei.site/pages/book_list.json')
 
