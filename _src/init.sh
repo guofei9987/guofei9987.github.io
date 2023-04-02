fb6d744ef887e7b381df56a674d27dcc8aaddaf9
robot <robot@guofei.site>
2023-04-02 12:43:36 +0000

    update

--- _src/init.sh
+++ _src/init.sh
@@ -1,3 +1,6 @@
 # 防止中文乱码
 git config --global core.quotepath false
 git config --global user.name "robot"
+git config --global user.email "robot@guofei.site"
+git config user.name "robot"
+git config user.email "robot@guofei.site"
