6541487fd1ba61d3411e2748c3e49ef1fd980220
robot <robot@guofei.site>
2025-12-13 11:00:00 +0800

    update

--- _drafts/py/commit.sh
+++ _drafts/py/commit.sh
@@ -3,6 +3,10 @@ cd a
 GIT_AUTHOR_DATE="$commitDate" GIT_COMMITTER_DATE="$commitDate" git commit -m "update"
 git push
 cd ..
+cd agent
+GIT_AUTHOR_DATE="$commitDate" GIT_COMMITTER_DATE="$commitDate" git commit -m "update"
+git push
+cd ..
 git add a
 GIT_AUTHOR_DATE="$commitDate" GIT_COMMITTER_DATE="$commitDate" git commit -m "update"
 git push
