9849f0b354e58c13699e97be038371e897515cc7
robot <robot@guofei.site>
2025-12-13 11:00:00 +0800

    update

--- _drafts/py/docker/build.sh
+++
@@ -1,5 +0,0 @@
-IMAGE_NAME=my-jekyll
-
-echo "📦 构建镜像：$IMAGE_NAME ..."
-docker build -t $IMAGE_NAME .
-echo "✅ 构建完成。"
--- _drafts/py/get_data.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-
-r = requests.get('https://www.guofei.site/get_data/achievement.json').text
-
-print(r)
-with open('_data/achievement.json', 'w') as f:
-    json.dump(json.loads(r), f, ensure_ascii=False, indent=0)
